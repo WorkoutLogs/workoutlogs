747d54e149eff8e8734c6a6450f42964e69b6481
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2023-12-23 20:04:50 -0300

    Added description

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import Content from "../components/content";
 
 const IndexPage = () => {
   const GooglePlayUrl = "https://play.google.com/store/apps/details?id=com.reddyyz.WorkoutLogs";
+  const description = 'Download WorkoutLogs app and start tracking your workouts today!'
 
   return (
     <Layout>
@@ -15,32 +16,44 @@ const IndexPage = () => {
         backgroundColor='#df2a27'
       />
       <Content>
-        <main style={{backgroundColor: "#df2a27"}}>
-          <div className="flex text-white p-4 flex-col md:flex-row">
-            <div className="flex-1 flex justify-center md:justify-end">
-              <div className="mr-8 flex flex-col">
-                <h1 className="text-6xl text-left w-64">The<br/>Best<br/>Workout<br/>Tracker.</h1>
-                <p className="text-xl mt-5" style={{maxWidth: 450}}>Download WorkoutLogs and start tracking your workouts today!</p>  
-                <GooglePlayButton
-                  url={GooglePlayUrl}
-                  theme='dark'
-                  className='mt-5 self-center md:self-auto'
-                />
-              </div>
-            </div>
-            <div className='flex-1 flex justify-center md:justify-start mt-8'>
-              <img
-                style={{
-                  width: 275
-                }}
-                className="ml-8"
-                src={AppScreenshot}
+        <div className="flex text-white p-4 flex-col md:flex-row" style={{backgroundColor: "#df2a27"}}>
+          <div className="flex-1 flex justify-center md:justify-end">
+            <div className="md:mr-8 flex flex-col">
+              <h1 className="text-6xl text-left w-64 font-bold">The<br/>Best<br/>Workout<br/>Tracker.</h1>
+              <p className="text-xl mt-5" style={{maxWidth: 450}}>{description}</p>  
+              <GooglePlayButton
+                url={GooglePlayUrl}
+                theme='dark'
+                className='mt-5 self-center md:self-auto'
               />
             </div>
           </div>
-        </main>
+          <div className='flex-1 flex justify-center md:justify-start mt-8'>
+            <img
+              style={{
+                width: 275
+              }}
+              className="md:ml-8"
+              src={AppScreenshot}
+            />
+          </div>
+        </div>
+        <div className="bg-white p-4">
+          <h1 className="text-2xl text-center mb-4 font-medium">App Functionalities</h1>
+          <p className="text-center mb-4">A simple workout tracker, developed by gym rats, for gym rats!</p>
+          <ul className="list-disc list-inside self-center">
+            <li>Track your workouts, record weight, reps, and RPE</li>
+            <li>Track all your body measurements (bodyweight, bicep size, etc.)</li>
+            <li>Plate calculator</li>
+            <li>Powerlifting points calculator</li>
+            <li>RPE/1RM calculator</li>
+            <li>Timers</li>
+          </ul>
+        </div>
       </Content>
-      <Footer/>
+      <Footer
+        backgroundColor='#df2a27'
+      />
     </Layout>
   )
 }
