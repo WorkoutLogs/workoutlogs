8b8fbac51ecb59388cbb48ad9f0e5d576341d01a
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2023-12-23 20:04:35 -0300

    Header and Footer updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-function Footer() {
-  return <footer className=''>
+function Footer({backgroundColor}) {
+  return <footer className='h-16 px-4 flex items-center text-white' style={{backgroundColor}}>
     <a href='https://workoutlogs.github.io/privacy-policy'>Privacy Policy</a>
   </footer>;
 }
--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React from 'react';
 // import { Container } from './styles';
 
 function Header({ backgroundColor }) {
-  return <header style={{backgroundColor}} className='text-white p-2 px-12'>
-    <a href='#' className='text-2xl'>WorkoutLogs</a>
+  return <header style={{backgroundColor}} className='text-white h-16 p-2 flex items-center md:px-12 md:block justify-center'>
+    <a href='#' className='text-2xl border-b-2 border-b-transparent hover:border-b-white'>WorkoutLogs</a>
   </header>;
 }
 
